ac34deed86eb562ac481f3704dc88de914c92c13
Daymel Bonne <7658145+dbonne@users.noreply.github.com>
2024-09-13 09:31:51 -0500

    chore(config): Add ignore stowrc file

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 set -x
 
-# Set dotfiles directory
-export STOW_DIR="$HOME"/Work/Development/code/github.com/dbonne/dotfiles
+VERSION=0.1.0-dev
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $CURRENT_DIR
 
 # restore dotfiles
 # stow -t "$HOME" alacritty
 stow -t "$HOME" git
-stow -t "$HOME" gnupg 
+stow -t "$HOME" gnupg
 # stow -t "$HOME" inputrc
 stow -t "$HOME" ssh
 stow -t "$HOME" starship
 stow -t "$HOME" tmux
 
 if [[ "$(uname)" == "Darwin" ]]; then
-    echo "You are on a macOS system."
-	stow -t "$HOME" zshrc
+  echo "You are on a macOS system."
+  stow -t "$HOME" zshrc
 else
-    echo "You are not on a macOS system."
-	stow -t "$HOME" bashrc
+  echo "You are not on a macOS system."
+  stow -t "$HOME" bashrc
 fi
