1f2bfcf503526b7d3a8b94983a4605c34c2eadd4
Daymel Bonne <7658145+dbonne@users.noreply.github.com>
2024-10-02 19:40:55 -0500

    chore: Update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -x
+
 # Set up XDG_CONFIG_HOME
-export XDG_CONFIG_HOME="$HOME"/.config
+export XDG_CONFIG_HOME=$HOME/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -14,24 +16,7 @@ create_directories() {
   done
 }
 
-packages=(
-  stow
-  fd
-  ripgrep
-  npm
-  lazygit
-  fzf
-)
-
-install_cmd=""
-
-# Change the installation command if we are in macOS
-if [[ "$(uname)" == "Darwin" ]]; then
-  install_cmd="brew install "
-else
-  install_cmd="sudo apt-get install -y "
-  sudo apt-get update
-fi
+install_cmd="sudo apt-get install -y "
 
 # Install packages using the command
 $install_cmd "${packages[@]}"
@@ -43,20 +28,4 @@ common_directories=(
 
 create_directories "${common_directories[@]}"
 
-# restore dotfiles
-stow -t "$HOME" git
-stow -t "$HOME" gnupg
-stow -t "$HOME" ssh
-stow -t "$HOME" tmux
-
-stow -t "$XDG_CONFIG_HOME/starship" starship
-stow -t "$XDG_CONFIG_HOME/nvim" nvim
-
-if [[ "$(uname)" == "Darwin" ]]; then
-  echo "You are on a macOS system."
-  stow -t "$HOME" zshrc
-else
-  echo "You are not on a macOS system."
-  stow -t "$HOME" bashrc
-  sudo rm -rf /var/lib/apt/lists/*
-fi
+stow -t $XDG_CONFIG_HOME/nvim nvim
